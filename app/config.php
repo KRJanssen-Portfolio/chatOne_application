471bad6b136d68ee9af10e875658ee6da751e613
keanu1997 <music-dude@hotmail.nl>
2016-06-25 17:08:36 +0200

    add the config.php file
    This file will give the application acces to the database

---
+++ app/config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KeanuenSabine
+ * Date: 20-6-2016
+ * Time: 09:30
+ */
+
+//1. constante aanmaken
+define('DBNAME', 'movie_app');
+define('DBUSER', 'root');
+define('DBHOST',  'localhost');
+define('DBPASS', '');
+
+//2. PDO verbinding maken met database
+//- geef eerst de database type
+//- dan de database naam
+
+try {
+    $db = new PDO("mysql:host=" . DBHOST . ";dbname=" . DBNAME, DBUSER, DBPASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
